5f38787601f44cd0cc4d34896ab002964392017b
André Costa <74073671+Andre-N-Costa@users.noreply.github.com>
2022-02-14 19:45:36 +0000

    Update frequencia.py

--- Treino/frequencia.py
+++ Treino/frequencia.py
@@ -1,19 +1,2 @@
-def last(n):
-    return n[1]
-
 def frequencia(texto):
-    sep = texto.split(" ")
-    sep = sorted(sep)
-    ocorr = []
-    new = []
-    i = 0
-    while i < len(sep):
-        if i == len(sep) - 1:
-            ocorr.append((sep[i],sep.count(sep[i])))
-        elif sep[i] != sep[i+1]:
-            ocorr.append((sep[i],sep.count(sep[i])))
-        i+=1
-    ocorr = sorted(ocorr, key = last, reverse = True)
-    for tup in ocorr:
-        new.append(tup[0])
-    return new
+    return sorted(list(set(texto.split())),key = lambda x : (len(texto.split()) - (texto.split()).count(x),x))
