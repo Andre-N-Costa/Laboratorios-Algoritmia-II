27e9c08130d8fd1609ba10719436fe3594cd9333
André Costa <74073671+Andre-N-Costa@users.noreply.github.com>
2022-02-16 19:25:33 +0000

    Update apelidos.py

--- Treino/apelidos.py
+++ Treino/apelidos.py
@@ -1,14 +1,2 @@
 def apelidos(nomes):
-    napelidos = []
-    new = []
-    j = 1
-    ord = sorted(nomes)
-    for i in range(len(nomes)):
-        napelidos.append(ord[i].count(" "))
-    while j <= max(napelidos):
-        while napelidos.count(j) != 0:
-            ind = napelidos.index(j)
-            napelidos[ind] = -1
-            new.append(ord[ind])
-        j+=1
-    return new
+    return sorted(nomes, key = lambda x : ((len(x.split())),x))
