2a5229815aa06b745d63a61775891228df246970
André Costa <74073671+Andre-N-Costa@users.noreply.github.com>
2022-02-16 20:07:54 +0000

    Update cruzamentos.py

--- Treino/cruzamentos.py
+++ Treino/cruzamentos.py
@@ -3,8 +3,6 @@ import string
 def cruzamentos(ruas):
     abc = string.ascii_lowercase
     cruz = []
-    new = []
-    ocorr = []
     for i in abc:
         no = 0
         for rua in ruas:
@@ -13,17 +11,9 @@ def cruzamentos(ruas):
             if rua[len(rua)-1] == i:
                 no += 1
         cruz.append((i,no))
-    if (cruz[len(cruz) - 1])[1] == 0:
-        cruz.pop(len(cruz) - 1)
-    for tup in cruz:
-        ocorr.append(tup[1])
-    for j in range(max(ocorr) + 1):
-        for tup in cruz:
-            if tup[1] == j:
-                new.append(tup)
-    for i,tup in enumerate(new):
-        if tup[1] == 0:
-            new.pop(i)
-    if (new[len(new) - 1])[1] == 0:
-        new.pop(len(new) - 1)
-    return new
+    cruz = sorted(cruz,key = lambda x: (x[1],x[0]))
+    a,b = cruz[0]
+    while b == 0:
+        cruz.pop(0)
+        a,b = cruz[0]
+    return cruz
