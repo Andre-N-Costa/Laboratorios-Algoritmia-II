263ce085aed79c61d11c498758a1d76ebc22ae22
André Costa <74073671+Andre-N-Costa@users.noreply.github.com>
2022-02-17 13:57:19 +0000

    Update frequencia.py

--- Treino/frequencia.py
+++ Treino/frequencia.py
@@ -1,2 +1,2 @@
 def frequencia(texto):
-    return sorted(list(set(texto.split())),key = lambda x : (len(texto.split()) - (texto.split()).count(x),x))
+    return sorted(list(set(texto.split())),key = lambda x : (-(texto.split()).count(x),x))
