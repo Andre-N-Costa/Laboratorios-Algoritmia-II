a3617f0f24f4287ef843b503448f5f5ad681b4ec
André Costa <74073671+Andre-N-Costa@users.noreply.github.com>
2022-03-26 18:52:48 +0000

    Update area.py

--- Treino2/area.py
+++ Treino2/area.py
@@ -1,38 +1,32 @@
-def bfs(adj,o):
-    pai = {}
-    vis = set()
-    vis.add(o)
+def adjs(p,mapa):
+    x,y = p
+    adj = []
+    if x < len(mapa)-1:
+        if mapa[x+1][y] != "*":
+            adj.append((x+1,y))
+    if x > 0:
+        if mapa[x-1][y] != "*":
+            adj.append((x-1,y))
+    if y < len(mapa)-1:
+        if mapa[x][y+1] != "*":
+            adj.append((x,y+1))
+    if y > 0:
+        if mapa[x][y-1] != "*":
+            adj.append((x,y-1))
+    return adj
+
+
+def bfs(mapa,o):
+    vis = {o}
     queue = [o]
     while queue:
         v = queue.pop(0)
-        for d in adj[v]:
+        for d in adjs(v,mapa):
             if d not in vis:
                 vis.add(d)
-                pai[d] = v
                 queue.append(d)
     return vis
 
-def construirG(mapa):
-    adj = {}
-    for y in range(len(mapa)):
-        for x in range(len(mapa)):
-            adj[(x,y)] = set()
-            if y > 0:
-                if mapa[x][y-1] != "*":
-                    adj[(x,y)].add((x,y-1))
-            if x > 0:
-                if mapa[x-1][y] != "*":
-                    adj[(x,y)].add((x-1,y))
-            if y < len(mapa)-1:
-                if mapa[x][y+1] != "*":
-                    adj[(x,y)].add((x,y+1))
-            if x < len(mapa)-1:
-                if mapa[x+1][y] != "*":
-                    adj[(x,y)].add((x+1,y))
-    return adj
-
-def area(p,mapa):
+def area(p, mapa):
     p = (p[1],p[0])
-    adj = construirG(mapa)
-    vis = bfs(adj,p)
-    return len(vis)
+    return len(bfs(mapa,p))
