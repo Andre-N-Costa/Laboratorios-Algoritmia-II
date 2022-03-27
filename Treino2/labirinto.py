af02f2d13af9df4a98ddb3bd71dec2ebf6430f80
André Costa <74073671+Andre-N-Costa@users.noreply.github.com>
2022-03-27 16:47:24 +0100

    Update labirinto.py

--- Treino2/labirinto.py
+++ Treino2/labirinto.py
@@ -2,23 +2,22 @@ def buildG(mapa):
     adj = {}
     for i in range(len(mapa)):
         for j in range(len(mapa)):
-            if mapa[i][j] != "#":
-                if (i,j) not in adj:
-                    adj[(i,j)] = set()
-                if i > 0 and mapa[i-1][j] != "#":
-                    adj[(i,j)].add((i-1,j))
-                if i < len(mapa)-1 and mapa[i+1][j] != "#":
-                    adj[(i,j)].add((i+1,j))
-                if j > 0 and mapa[i][j-1] != "#":
-                    adj[(i,j)].add((i,j-1))
-                if j < len(mapa)-1 and mapa[i][j+1] != "#":
-                    adj[(i,j)].add((i,j+1))
+            if (i,j) not in adj:
+                adj[(i,j)] = []
+            if i > 0 and mapa[i-1][j] != "#":
+                adj[(i,j)].append((i-1,j))
+            if i < len(mapa)-1 and mapa[i+1][j] != "#":
+                adj[(i,j)].append((i+1,j))
+            if j > 0 and mapa[i][j-1] != "#":
+                adj[(i,j)].append((i,j-1))
+            if j < len(mapa)-1 and mapa[i][j+1] != "#":
+                adj[(i,j)].append((i,j+1))
     return adj
-                
-def bfs(adj):
+    
+def bfs(adj,o):
     pai = {}
-    vis = {(0,0)}
-    queue = [(0,0)]
+    vis = {o}
+    queue = [o]
     while queue:
         v = queue.pop(0)
         for d in adj[v]:
@@ -27,25 +26,22 @@ def bfs(adj):
                 pai[d] = v
                 queue.append(d)
     return pai
-        
 
 
 def caminho(mapa):
     o = (0,0)
-    adj = buildG(mapa)
-    pai = bfs(adj)
-    point = (len(mapa)-1,len(mapa)-1)
-    cardeais = []
-    while point != (0,0):
-        x,y = point
-        if pai[point] == (x-1,y):
-            cardeais.append("S")
-        elif pai[point] == (x+1,y):
-            cardeais.append("N")
-        elif pai[point] == (x,y-1):
-            cardeais.append("E")
-        elif pai[point] == (x,y+1):
-            cardeais.append("O")
-        point = pai[point]
-    cardeais.reverse()
-    return "".join(cardeais)
+    pais = bfs(buildG(mapa),o)
+    d = (len(mapa)-1,len(mapa)-1)
+    cardeais = ""
+    while(d != o):
+        x,y = d
+        if pais[d] == (x+1,y):
+            cardeais+="N"
+        if pais[d] == (x-1,y):
+            cardeais+="S"
+        if pais[d] == (x,y+1):
+            cardeais+="O"
+        if pais[d] == (x,y-1):
+            cardeais+="E"
+        d = pais[d]
+    return cardeais[::-1]
