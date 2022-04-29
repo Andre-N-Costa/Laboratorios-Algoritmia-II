f315fcafe43cda0729cd5c7a6cdd400c7462f73e
André Costa <74073671+Andre-N-Costa@users.noreply.github.com>
2022-04-29 23:41:11 +0100

    Update ladrao.py

--- Treino3/ladrao.py
+++ Treino3/ladrao.py
@@ -1,14 +1,21 @@
 """
-80%
+90%
 """
 
 def ladrao(capacidade,objetos):
+    return ladraoaux(capacidade,objetos,{})
+
+def ladraoaux(capacidade,objetos,d):
+    r = float("-inf")
     if objetos == []:
         return 0
     if capacidade == 0:
+        d[capacidade] = 0
         return 0
-    r = float("-inf")
+    if capacidade-3 in d:
+        return d[capacidade-3]
     for count,m in enumerate(objetos):
         if m[2] <= capacidade:
-            r = max(r,m[1]+ladrao(capacidade - m[2],objetos[count+1:]))
+            r = max(r,m[1]+ladraoaux(capacidade - m[2],objetos[count+1:],d))
+    d[capacidade] = r 
     return r
