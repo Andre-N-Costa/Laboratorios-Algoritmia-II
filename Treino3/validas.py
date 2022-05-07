4d13c9eb87d386bff3cb0b3dedcce5613f2f6441
André Costa <74073671+Andre-N-Costa@users.noreply.github.com>
2022-05-07 17:58:45 +0100

    Create validas.py

---
+++ Treino3/validas.py
@@ -0,0 +1,27 @@
+"""
+90%
+"""
+
+def validasaux(soma,lista,elem):
+    if lista == []:
+        return []
+    valor = elem
+    for i in lista:
+        if valor + i == soma:
+            return True
+        elif valor + i < soma:
+            valor+=i
+    if valor == soma:
+        return True
+    return False
+
+def validas(soma,listas):
+    if listas == []:
+        return []
+    final = []
+    for lista in listas:
+        for elem in range(len(lista)):
+            if validasaux(soma,lista[:elem] + lista[elem+1:],lista[elem]):
+                final.append(lista)
+                break
+    return final
