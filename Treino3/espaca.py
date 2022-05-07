956725bcb5f8efa2292cb8d2ec90efeb875f0762
André Costa <74073671+Andre-N-Costa@users.noreply.github.com>
2022-05-07 19:15:20 +0100

    Update espaca.py

--- Treino3/espaca.py
+++ Treino3/espaca.py
@@ -1,30 +1,46 @@
 """
-50%
+70%
 """
 
 def espaca(frase,palavras):
-    return espacaaux(frase,palavras,0)
+    return espacaaux(frase,sorted(palavras, key = lambda x: -len(x)),0)
 
 
 def espacaaux(frase,palavras,n):
-    if n == len(frase):
+    if palavras == []:
         return ""
-    palavras = sorted(palavras, key = lambda x: -len(x))
     i = 0
     j = 0
     aux = n
-    while aux < len(frase) and j < len(palavras):
-        if i == 0:
-            while(frase[aux] != palavras[j][i]):
+    final = []
+    while n != len(frase):
+        while aux < len(frase) and j < len(palavras):
+            if i == 0:
+                while(frase[aux] != palavras[j][i]):
+                    j+=1
+            if frase[aux] == palavras[j][i]:
+                if i == len(palavras[j])-1:
+                    j1 = 0
+                    if aux+1 < len(frase):
+                        while(j1 < len(palavras) and frase[aux+1] != palavras[j1][0]):
+                            j1+=1
+                        if j1 == len(palavras):
+                            i = 0
+                            aux = n
+                            j+=1
+                        else:
+                            break
+                    else:
+                        break
+                i+=1
+                aux+=1
+            else:
+                i = 0
+                aux = n
                 j+=1
-        if frase[aux] == palavras[j][i]:
-            if i == len(palavras[j])-1:
-                break
-            i+=1
-            aux+=1
-        else:
-            i = 0
-            aux = n
-            j+=1
-    print(palavras[j])
-    return (palavras[j] + " " + espacaaux(frase,palavras,aux+1)).rstrip()
+        final.append(palavras[j])
+        i = 0
+        j = 0
+        aux+=1
+        n = aux
+    return " ".join(final)
