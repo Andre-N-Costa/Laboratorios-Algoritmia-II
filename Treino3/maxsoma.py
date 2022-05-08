8344f5ac7e7a6d1f1bafec0878f7f37e3c52c3e6
André Costa <74073671+Andre-N-Costa@users.noreply.github.com>
2022-05-08 22:29:41 +0100

    Update maxsoma.py

--- Treino3/maxsoma.py
+++ Treino3/maxsoma.py
@@ -1,19 +1,18 @@
 """
-80%
+90%
 """
 def maxsoma(lista):
-    if lista == []:
-        return 0
-    if len(lista) == 1:
-        return lista[0]
     somas = []
     final = []
     somas.append(lista[0])
     for n in lista[1:]:
         somas.append(n + somas[-1])
     final.append(max(somas))
-    for n in range(len(lista)-1):
+    print(somas)
+    print(final)
+    for n in range(len(lista)):
         somas = list(map(lambda x: x - somas[0],somas))
-        final.append(max(somas))
         somas.pop(0)
+        if somas != []:
+            final.append(max(somas))
     return max(final)
